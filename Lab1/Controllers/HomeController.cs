6e54d0a1f8196ca66500a7094032193dad8b6fbd
WillyForest <kinashchuksasha@gmail.com>
2018-10-29 22:45:17 +0200

    user roles added

--- Lab1/Controllers/HomeController.cs
+++ Lab1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,10 +8,50 @@ using System.Web.Mvc;
 namespace Lab1.Controllers
 {
     public class HomeController : Controller
-    {
-        public ActionResult Index()
+	{													  
+		private ApplicationUserManager _userManager;
+		
+		public HomeController()
+		{
+		}
+
+		public HomeController(ApplicationUserManager userManager)
+		{
+			UserManager = _userManager;	  
+		}
+
+		public ApplicationUserManager UserManager
+		{
+			get
+			{
+				return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+			}
+			private set
+			{
+				_userManager = value;
+			}
+		}
+
+		public ActionResult Index()
         {
-            return View();
+			try
+			{
+				var admin = UserManager.IsInRoleAsync(Microsoft.AspNet.Identity.IdentityExtensions.GetUserId(User.Identity), "Admin");
+				if (admin.Result)
+				{
+					ViewBag.Message = "Your role is admin";
+				}
+				else
+				{
+					ViewBag.Message = "Your role is not admin";
+				}
+			}
+			catch (Exception ex)
+			{
+
+			}
+
+			return View();
         }
 
         public ActionResult About()
